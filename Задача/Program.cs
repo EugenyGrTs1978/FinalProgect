fdb9c8fa2eaddcddfa8a3f78f64b25b24623e8a1
EugenyTsypyshev <egtsypyshev@mail.ru>
2022-11-11 19:28:14 +0500

    Добавление решения

---
+++ Задача/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] OutputArray(string[] InputArray)
+{
+    int count = 0;
+    for (int i = 0; i < InputArray.Length; i++)
+    {
+        if (InputArray[i].Length <=3)
+            {
+                count++;
+            }
+          
+    }
+    string[] arr = new string[count];
+    int index = 0;
+    for (int i = 0; i < InputArray.Length; i++)
+    {
+        if (InputArray[i].Length <= 3)
+            {
+                arr[index] = InputArray[i];
+                index++;
+            }
+    }
+    return arr;
+}
+
+string[] array = {"Moscow","House","45","dog","I"};
+string[] UserArray = OutputArray(array);
+Console.WriteLine('['+string.Join(',',UserArray)+']');
